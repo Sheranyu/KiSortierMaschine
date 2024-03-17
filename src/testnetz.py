daa4cb8ea5f4328ebaa8d296f3e8bd3c23c30a08
Sheranyu <126327508+Sheranyu@users.noreply.github.com>
2024-03-17 17:37:16 +0100

    erstes einfaches testnetz

--- src/testnetz.py
+++ src/testnetz.py
@@ -46,8 +46,14 @@ class MeinNetz(nn.Module):
         x = self.conv2(x)
         x = self.conv_dropout(x)
         
-        x = F.max_pool2d(x)
+        x = F.max_pool2d(x,2)
         x = F.relu(x)
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x)
+        
+        
         return x
     
     def num_flat_features(self,x):
@@ -79,8 +85,32 @@ def train(epoch: int):
         print("Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
             epoch, batch_id * len(data), len(trainloader.dataset),
             100. * batch_id / len(trainloader), loss.item()))
+    torch.save(model,"meinmodel.pt")
 
-for epoch in range(30):
-    train(epoch)
+def test():
+    model.eval()
+    loss = 0
+    n_samples = len(testloader.dataset)
+    correct = 0
+    for data, target in testloader:
+        data = Variable(data, volatile=True)
+        out = model(data)
+        loss += F.nll_loss(out,target, size_average=False).data.item()
+        # pred = out.data.max(1,keepdim=True)[1]
+        # correct += pred.eq(target.data.view_as(pred)).sum()
+        _, predicted = torch.max(out, 1)
         
+        correct += (predicted == target).sum().item()
+        print(correct.item())
+        correct = correct.item()
+    # acc = correct / n_samples
+    # print(f'Accuracy of the network on the {n_samples} test images: {100*acc} %')  
+    # loss = loss/len(testloader.dataset)
+    # print("Durchschnittloss: ", loss)
+        
+        
+model = torch.load("meinmodel.pt")
+for epoch in range(1,5):
+   # train(epoch)
+    test()    
 
