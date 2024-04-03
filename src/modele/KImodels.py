954e9f0cac0088acfdc2c04e5ac52195ea82848e
Sheranyu <126327508+Sheranyu@users.noreply.github.com>
2024-04-03 17:33:04 +0200

    modelchange

--- src/modele/KImodels.py
+++ src/modele/KImodels.py
@@ -10,9 +10,10 @@ class MeinNetz(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
         self.conv2 = nn.Conv2d(6, 12, kernel_size=5)
         self.conv3 = nn.Conv2d(12, 18, kernel_size=5)
-        self.fc1 = nn.Linear(18 * 28 * 28, 1500)  # 18 channels * 4x4 spatial dimensions
+        self.conv4 = nn.Conv2d(18, 24, kernel_size=5)  # Neu hinzugefügte Convolutional-Schicht
+        self.fc1 = nn.Linear(24 * 12 * 12, 1500)  # Aktualisierte Eingangsgröße für die erste lineare Schicht
         self.fc2 = nn.Linear(1500, outputs)
-        self.dropout = nn.Dropout(p=0.2)  # Dropout mit einer Wahrscheinlichkeit von 0.5
+        self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -24,8 +25,13 @@ class MeinNetz(nn.Module):
         x = self.conv3(x)
         x = F.max_pool2d(x, 2)
         x = F.relu(x)
-        x = x.view(-1, 18 * 28 * 28)  # Reshape, um die richtige Größe für die lineare Schicht zu erhalten
+        x = self.conv4(x)  # Neue Convolutional-Schicht
+        x = F.max_pool2d(x, 2)
+        x = F.relu(x)
+        print(x.size())
+        exit()
+        x = x.view(-1, 24 * 12 * 12)  # Aktualisierter Reshape für die erste lineare Schicht
         x = F.relu(self.fc1(x))
-        x = self.dropout(x)  # Dropout-Schicht angewendet
+        x = self.dropout(x)
         x = self.fc2(x)
         return x
